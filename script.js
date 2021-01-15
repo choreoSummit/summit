ffa47c7a804f8671cad4620e5232c34cdae9c3ef
Calvin <calvin.barrett@hotmail.com>
2021-01-15 15:30:47 -0600

    main page adjustments

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ mobileNav = function () {
   const topLine = document.querySelector('.hamburger span:nth-of-type(1)');
   const middleLine = document.querySelector('.hamburger span:nth-of-type(2)');
   const bottomLine = document.querySelector('.hamburger span:nth-of-type(3)');
+  const nav = document.querySelector('ul');
+  const logo = document.querySelector('nav img');
+  const welcome = document.querySelector('h2');
 
 
   // toggle the nav class and transform the hamburger
@@ -13,6 +16,9 @@ mobileNav = function () {
     topLine.classList.toggle('line1');
     middleLine.classList.toggle('line2');
     bottomLine.classList.toggle('line3');
+    nav.classList.toggle('open');
+    logo.classList.toggle('hide');
+    welcome.classList.toggle('hide');
   });
 };
 
