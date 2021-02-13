5e72f491a48559c4445bfa2ff778ffcbba3954ca
Harry <harryguloien@gmail.com>
2021-02-13 17:30:18 -0500

    choreo page function

--- script.js
+++ script.js
@@ -1,19 +1,211 @@
+const bios = [
+  {
+    "name": "ANGELO SAUNDERS",
+    "bio": "All the way from Philly, Angelo Saunders brings a fire to the CS Faculty. He has been dancing and training in the industry for 5 years, bringing along with him his experience working with music icons, Beyoncé, Pharrell and Kendrick Lamar. His style is not only reflected in his choreography, but also his closest. He blesses our Instagram timelines with the freshest fits and he never misses. Angelo kicked off our first ever Choreo Summit in 2019, opening the weekend with high energy and blessed the studio with exclusive knowledge for the AZ community."
+  },
+
+  {
+    "name": "ANTWANE YOUNGER",
+    "bio": "Pittsburgh brings us Antwane Younger, who began his dance career under the mentorship of Gabriel Ash. Antwane was the assistant director of KG Dynasty and taught at Millennium Dance Complex Pittsburgh, and was a member of “The Company” at MDCP. His artist credits include Marques Houston, Jacquees and Avant. Twane never fails to bring his infectious high energy any time he enters a room and hopes to use his platform to shine light on the Pittsburgh talent."
+  },
+
+  {
+    "name": "CAMERON “MISFIT” GRAHAM",
+    "bio": "Originally from Compton, California, Cameron “Misfit” Graham has been training in the dance industry for 12 years. With his training time came his expertise which brought him to teaching in California and across the nation. Cameron’s teaching credits include Boogiezone Utopia, Soul De Soul Dance Convention, The Streetz Dance Convention, Band of Misfits, and Kodachrome Workshops. He has spent time as a working dancer in the film and TV industry being featured in Step Up 3D, WILL.I.AM’s “Super Dope”, also working with brands such as Facebook, Nike, Footlocker, and Volkswagon. “Misfit” will be joining us for our new youth training session for the first time in 2020."
+  },
+
+  {
+    "name": "CISCO",
+    "bio": "We are honored to bring on Arizona’s one and only Cisco to our Choreo Summit line-up for 2020. He is one of our most versatile choreographers holding credits with top industry artists such as Jennifer Lopez, Lil Wayne, Pitbull, Neyo, Saweetie, Tinashe, and Lyrica Anderson. His commercial work includes Honda, Tiffany & Co., and Dancing with the Stars. Cisco has been training for 16 years and has been working in LA for 6 years. Within these 6 years he has established himself as a well respected choreographer known for his heels class and brand “Purr Movement”. But his talents don’t stop there, he is also well-versed in hip hop, is active in the vogue community, and is a videographer and creative director."
+  },
+  {
+    "name": "DALLAS TRILL",
+    "bio": "Choreo Summit welcomes back faculty Dallas Trill from Santa Ana, California for our 2020 Summit. Dallas returns to AZ often to enrich the community and nurture his loving relationship that he has built there. He has been dancing for 16 years and has been working in the industry for the past 3 and a half years with credits including; Kanye West Sunday Service- Coachella, Jamie Boy, and Isn’t It Romantic movie promo. As a part of Ian Eastwood’s “Young Lions” he has had the opportunity to work for American Eagle and was featured in the “Earthling VIP” video for Biggie Smalls. Dallas shares his wealth of knowledge with the AZ community and remains an outlet to the industry for dancers of all ages."
+  },
+
+  {
+    "name": "GABRIEL “G” ASH",
+    "bio": "Gabriel “G” Ash is a well respected creative director, choreographer, and dancer originally from Pittsburgh, Pennsylvania. His 13 years of training has brought him expertise in various styles from hip hop, to ballet, jazz, and contemporary. Currently residing in Los Angeles where he was signed to Bloc Talent Agency, you can find him teaching regularly at Millenium Dance Complex and IMMA Space. Some of his credits include principal dancer for the TV series Howard High, Teyana Taylor, Miguel, Lloyd, Marques Houston, IMX, is two time winner of Show Time at the Apollo, and was a semi-finalist on Fox’s So You Think You Can Dance. We are excited to bring him back to teach in our youth session for Choroe Summit 2020."
+  },
+
+  {
+    "name": "FACEOFF",
+    "bio": "Originally from Boston, Massachusetts, FaceOff brought his unique movement to the very first Choreo Summit event in the fall of 2019. He has been dancing since he was 12 years old and you can now find him in Los Angeles, where he currently resides, teaching at KreativMindz Dance Complex. He regularly features his work at various dance choreography showcases and is an active member in the LA dance scene. FaceOff has worked with respected artists such as Trevor Jackson, Jermain Elliot, KPop artist, Elly, and Kamaiyah in the music video and movie industries. His class challenges his students to pick up and adapt to his movement while finding individuality in the choreography and we can’t wait to bring him back for a future Summit."
+  },
+
+  {
+    "name": "HANNAH OSBORN",
+    "bio": "(bio here)"
+  },
+
+
+  {
+    "name": "HUNTER MCCABE",
+    "bio": "(bio here)"
+  },
+
+  {
+    "name": "KAILI BRIGHT",
+    "bio": "Kaili Bright will be the first woman teaching at Choreo Summit. We are excited to bring her energy home to Arizona for the 2021 event. She has been dancing for 22 years and has been a working dancer in the industry for 10, having experience with some of our all time favorite artists: Rihanna, Ariana Grande, Justin Bieber, Pink, J.Lo, Kelly Clarckson and Jason Derulo. Kaili’s travels have landed her in Dallas, Texas where she is now a partner with The Joffrey Ballet School, teaching master classes, and also running her own program, The Vision Project with esteemed colleague Robert “Slim” Hernandez. Kaili is an idol for dancers of all ages and we are excited to bring her knowledge to the forefront for our Summit dancers."
+  },
+
+  {
+    "name": "KYLA FAJARDO",
+    "bio": "(bio here)"
+  },
+
+  {
+    "name": "KYRI WILLIAMS",
+    "bio": "Kyri Williams is a Long Beach, CA native that has cultivated a community for individuals to level up in their early dance careers. He has created his own training program, Society, in which he brings on a vast group of experienced creatives to help enrich his students in all aspects of the entertainment industry. He shares his wealth of information from the 11 years dancing and 9 working in the dance industry with credits that include Meghan Trainor, Bobby Brown, MTV, Lily Singh, and Disney. Kyri’s training program has inspired us to create our very own elite level package, coming to Choreo Summit 2020."
+  },
+
+
+  {
+    "name": "MATTHEW DESHONE HARRIS",
+    "bio": "Born in the ATL, Matthew moved down to Billings, Montana where he began dancing. He has been living in Los Angeles for the past 5 years and has been training in the industry for 5 in hip hop, jazz funk, and industry stage technique. Matthew went into artist development in 2017 and worked with this first client, Terrance Wilde with Mark Fiest and Stephen Banks. You might have seen him on America’s Got Talent, season 12 with the Brobots and Mandroidz."
+  },
+
+  {
+    "name": "SHAMORIA LIMAE",
+    "name": "(bio here)"
+  },
+  {
+    "name": "TIMOTHY HOWARD",
+    "bio": "Hailing from the Southside of Chicago, Timothy Howard has been dancing for 13 years, training for 8, and working in the dance industry for 3. He has worked primarily as a dancer with credits with the hit show Empire, Keke Palmer, Post Malone, and Google Pixel. Tim’s choreography recently made an appearance on the new Quibi show “Floored” where he also performed it. He has been in Los Angeles for a few years also shooting concepts and working closely with his mentor Chuck Maldonado at Fuzion Force LA. Timothy has the honor of being one of our teachers from the first Summit and he can’t wait to watch our movement grow."
+  }
+
+]
 // const app = {};
 
-mobileNav = function () {
-  const hamburger = document.querySelector('.hamburger');
-  const topLine = document.querySelector('.hamburger span:nth-of-type(1)');
-  const middleLine = document.querySelector('.hamburger span:nth-of-type(2)');
-  const bottomLine = document.querySelector('.hamburger span:nth-of-type(3)');
+// mobileNav = function () {
+//   const hamburger = document.querySelector('.hamburger');
+//   const topLine = document.querySelector('.hamburger span:nth-of-type(1)');
+//   const middleLine = document.querySelector('.hamburger span:nth-of-type(2)');
+//   const bottomLine = document.querySelector('.hamburger span:nth-of-type(3)');
 
 
-  // toggle the nav class and transform the hamburger
-  hamburger.addEventListener('click', function () {
-    console.log('hi');
-    topLine.classList.toggle('line1');
-    middleLine.classList.toggle('line2');
-    bottomLine.classList.toggle('line3');
+//   // toggle the nav class and transform the hamburger
+//   hamburger.addEventListener('click', function () {
+//     console.log('hi');
+//     topLine.classList.toggle('line1');
+//     middleLine.classList.toggle('line2');
+//     bottomLine.classList.toggle('line3');
+//   });
+// };
+
+// mobileNav();
+
+
+
+
+
+console.log(bios);
+
+const dancerElem = document.querySelectorAll('.dancer');
+const dancerImage = document.querySelectorAll('.dancer img');
+
+//highlights mini image + un-highlights all others
+function chosenHighlight(chosenDancer) {
+  dancerImage.forEach(image => {
+    if (image.classList.contains('chosen-highlight')) {
+      image.classList.remove('chosen-highlight');
+    }
   });
-};
+  chosenDancer.classList.add('chosen-highlight');
+}
 
-mobileNav();
+//displays artist data for mouse hover
+function getTargetMouse(event) {
+  if(event.target.firstChild) {
+    let chosenDancer = event.target.firstChild
+    putDataOnScreen(chosenDancer);
+
+
+    console.log(chosenDancer);
+  }
+}
+
+function putDataOnScreen(chosenDancer) {
+  let displayedBio;
+  let imgSrc = chosenDancer.getAttribute('src');
+  let dName = chosenDancer.getAttribute('alt');
+  let nameCompare = chosenDancer.getAttribute('data-name')
+  bios.forEach(bio => {
+    if (bio.name.toLowerCase().includes(nameCompare.toLowerCase())) {
+      displayedBio = bio.bio;
+    }
+  })
+
+  document.getElementById('d-image').src = imgSrc;
+  document.getElementById('name').innerHTML = dName;
+  document.getElementById('dancer-bio').innerHTML = displayedBio;
+  // console.log(displayedBio)
+}
+
+//displays artist data  + highlights choice for click
+function displayChosen(event) {
+    let chosenDancer = event.target.firstChild
+    getTargetMouse(event)
+    chosenHighlight(chosenDancer);  
+}
+
+//displays artist data + highlights choice for ENTER key
+function enterToDisplayChosen(event) {
+  if (event.keyCode == 13) {
+    let chosenDancer = event.target;
+    getKeyboardTarget(event);
+    chosenHighlight(chosenDancer);
+  }
+}
+
+//removes mouseover and tab through event listeners and adds highlight + display (click and enter keys)
+function isClicked() {
+  dancerElem.forEach(item => {
+    item.removeEventListener('mouseover', getTargetMouse);
+    item.removeEventListener('keyup', tabThrough);
+    item.addEventListener('click', displayChosen);
+    item.addEventListener('keyup', enterToDisplayChosen);
+  })
+}
+
+//adding intital event listeners for hover, click, tab & enter keys
+dancerElem.forEach(item => {
+  item.addEventListener('mouseover', getTargetMouse);
+  item.addEventListener('click', isClicked);
+
+  // Accessibility with tab and enter keys
+  item.addEventListener('keyup', tabThrough);
+  item.addEventListener('keydown', enterToSelect);
+})
+
+
+//checks for ENTER key press an runs isClicked
+function enterToSelect(e) {
+    if (e.keyCode == 13) {
+      isClicked(e);
+    }
+}
+
+//checks for TAB key press and previews 
+function tabThrough(e) {
+    if (e.keyCode == 9) {
+      getKeyboardTarget(e);
+    }
+  };
+
+//Grabs target for keyboard + display data (for TAB key)
+function getKeyboardTarget(event) {
+  let chosenDancer = event.target
+  putDataOnScreen(chosenDancer);
+}
+
+const getBio = (dancerImgTag) => {
+  console.log(dancerImgTag.getAttribute)
+
+}
+
+
+
+
+
+
